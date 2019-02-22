733b0efdc4e6d0b003483fb37390bd1125af8fad
John Lindquist <johnlindquist@gmail.com>
2019-02-22 13:45:25 -0700

    draw

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
-function* generateAlphabet() {
-  let i = "a".charCodeAt(0)
-  let end = "z".charCodeAt(0) + 1
+document.body.style.margin = 0
 
-  while (i < end) {
-    yield String.fromCharCode(i)
-    i++
+let canvas = document.createElement("canvas")
+document.body.appendChild(canvas)
+
+canvas.width = document.body.clientWidth
+canvas.height = document.body.clientHeight
+
+let context = canvas.getContext("2d")
+context.lineWidth = 10
+context.lineCap = "round"
+
+let isMouseDown = false
+let previous = { x: 0, y: 0 }
+
+canvas.addEventListener("mousemove", event => {
+  if (isMouseDown) {
+    let { pageX: x, pageY: y } = event
+    context.beginPath()
+    context.moveTo(previous.x, previous.y)
+    context.lineTo(x, y)
+    context.stroke()
+
+    previous = { x, y }
   }
-}
+})
 
-let alphabet = [...generateAlphabet()]
+canvas.addEventListener("mousedown", event => {
+  let { pageX: x, pageY: y } = event
+  previous = { x, y }
+
+  isMouseDown = true
+})
+
+canvas.addEventListener("mouseup", event => {
+  isMouseDown = false
+})
